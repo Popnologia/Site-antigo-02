579aa27b3d1fe4fad1a3450066cf5519f9b05ee2
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2023-08-08 20:21:49 -0300

    Painel lateral
    Criação do painel lateral para armazenar botão de mostrar/esconder nav e modo claro/escuro.

--- docs/scripts/nav.js
+++ docs/scripts/nav.js
@@ -1,12 +1,12 @@
 function navMobile() {
   let botaoAbrir = window.document.querySelector("i#botao-nav-abrir")
   let botaoFechar = window.document.querySelector("i#botao-nav-fechar")
-  let menuNav = window.document.querySelector("div#nav-list")
+  let menuNav = window.document.querySelector("nav")
 
   if (botaoAbrir.style.display != "none") {
     botaoAbrir.style.display = "none"
     botaoFechar.style.display = "block"
-    menuNav.style.display = "block"
+    menuNav.style.display = "flex"
   } else {
     botaoAbrir.style.display = "block"
     botaoFechar.style.display = "none"
